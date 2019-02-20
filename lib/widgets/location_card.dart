70646dce488326e773ab033c5c462e53f3f6c771
plokmij <samfan@live.com>
2019-02-20 23:18:19 +0530

    add navbar

---
+++ lib/widgets/location_card.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LocationCard extends StatelessWidget {
+  final String location;
+  LocationCard({this.location});
+
+  Widget build(BuildContext context) {
+    return Text('Under construction'); 
+  }
+
+  Widget card(){
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
