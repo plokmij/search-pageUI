964d3a863a8eaccbc24c4dc7d50e5d6ba3f9474b
plokmij <samfan@live.com>
2019-02-21 00:50:13 +0530

    add filter option cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'widgets/location_card.dart';
 
 void main() {
   runApp(App());
@@ -9,26 +10,57 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
       home: CupertinoPageScaffold(
-        child: Center(
-          child: Text('Hello fucken world'),
-        ),
         navigationBar: CupertinoNavigationBar(
-          leading: Icon(Icons.arrow_back,
-          color: Colors.black,),
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
           middle: CupertinoTextField(
             placeholder: 'Search',
             decoration: BoxDecoration(
-              color: Colors.white10,
-              border: Border.all(
-                width: 1.0,
-                color: Colors.grey,
-              )
-            ),
+                color: Colors.white10,
+                border: Border.all(
+                  width: 1.0,
+                  color: Colors.grey,
+                )),
           ),
-          trailing: Icon(Icons.remove_red_eye,
-          color: Colors.black,),
+          trailing: Icon(
+            Icons.remove_red_eye,
+            color: Colors.black,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            buildList(),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text(
+                'I m going kdfkl dfjkh sdkfjhskjdf hksjjdfh ksdhfklsdjhf ksdjjhf alksjdfhlkjsadhf klsjdjhfksdjhf klsadjjfh ksdjfhksdjfhksajdhfks dfklsadfhlksajdhf lksjdhf lksdjhf lksdjhfkzxcmzxbnclkash d lkasjdhfks zfkjasdhf kljsghdfk;asjhd fkljsd kfjhs adlfkjas dfkjh sakdjfh skdfhskdf klsajdfh skjdfhksdhfks vmzxcbnsdhgf kjasdf kasdhf crazy'),
+          ],
         ),
       ),
     );
+  }
+
+  Widget buildList() {
+    return Container(
+        height: 100.0,
+        margin: EdgeInsets.only(top: 75.0),
+        padding: EdgeInsets.all(5.0),
+        child: ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                LocationCard(),
+                LocationCard(),
+                LocationCard(),
+                LocationCard()
+              ],
+            )
+          ],
+        ));
   }
 }
--- lib/widgets/location_card.dart
+++ lib/widgets/location_card.dart
@@ -5,14 +5,32 @@ class LocationCard extends StatelessWidget {
   LocationCard({this.location});
 
   Widget build(BuildContext context) {
-    return Text('Under construction'); 
+    return card(); 
   }
 
   Widget card(){
     return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5.0),
       child: Stack(
         children: <Widget>[
-          
+          colorBox(Colors.blueAccent),
+        ],
+      ),
+    );
+  }
+
+  Widget colorBox(Color color){
+    return Container(
+      height: 80.0,
+      width: 120.0,
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.rectangle,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            blurRadius: 2.0,
+            offset: (Offset(0.0, 2.0)),
+          ),
         ],
       ),
     );
