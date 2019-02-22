40c947234ebdde5a26b5d529b1a27da83fae5108
plokmij <samfan@live.com>
2019-02-22 14:57:49 +0530

    Minor UI Changes

--- lib/main.dart
+++ lib/main.dart
@@ -42,41 +42,67 @@ class App extends StatelessWidget {
 
   Widget buildBody() {
     return Container(
+      /*decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Colors.red, width: 10.0)
+        ),
+      ),*/
+      alignment: Alignment.topCenter,
       height: 498.0,
       child: ListView(
+        padding: EdgeInsets.only(top: 15.0),
+        shrinkWrap: true,
         scrollDirection: Axis.vertical,
         children: <Widget>[
           Column(
             children: <Widget>[
-              Text(
-                'Centres in Location 1',
-                style: TextStyle(
-                  fontSize: 21.0,
-                  fontWeight: FontWeight.w700,
+              Container(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  'Centres in Location 1',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
+                alignment: Alignment.topLeft,
               ),
               buildSearchTile(),
-              Text(
-                'Centres in Location 2',
-                style: TextStyle(
-                  fontSize: 21.0,
-                  fontWeight: FontWeight.w700,
+              Container(
+                padding: EdgeInsets.only(left: 10.0,top:10.0),
+                child: Text(
+                  'Centres in Location 2',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
+                alignment: Alignment.topLeft,
               ),
               buildSearchTile(),
-              Text(
-                'Centres in Location 3',
-                style: TextStyle(
-                  fontSize: 21.0,
-                  fontWeight: FontWeight.w700,
+              Container(
+                padding: EdgeInsets.only(left: 10.0,top:10.0),
+                child: Text(
+                  'Centres in Location 2',
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
+                alignment: Alignment.topLeft,
               ),
-              buildSearchTile(),Text(
-                'Centres in Location 3',
-                style: TextStyle(
-                  fontSize: 21.0,
-                  fontWeight: FontWeight.w700,
+              buildSearchTile(),
+              Container(
+                padding: EdgeInsets.only(left: 10.0,top:10.0),
+                child: Text(
+                  'Centres in Location 3',
+                  //textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 21.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
+                alignment: Alignment.topLeft,
               ),
               buildSearchTile(),
             ],
@@ -91,22 +117,25 @@ class App extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.white,
           boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: Colors.black38,
-            blurRadius: 1.0,
-            offset: (Offset(0.0, 2.0)),
-          ),
-        ],
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 3.0,
+              offset: (Offset(0.0, 3.0)),
+            ),
+          ],
         ),
         height: 100.0,
-        margin: EdgeInsets.only(top: 75.0),
-        padding: EdgeInsets.all(5.0),
+        margin: EdgeInsets.only(top: 67.5),
+        padding: EdgeInsets.only(top: 5.0, bottom: 5.0, right: 5.0),
         child: ListView(
           shrinkWrap: true,
           scrollDirection: Axis.horizontal,
           children: <Widget>[
             Row(
               children: <Widget>[
+                SizedBox(
+                  width: 10.0,
+                ),
                 LocationCard(
                   location: "Wandoor",
                   color: Colors.grey,
@@ -129,8 +158,10 @@ class App extends StatelessWidget {
 
   Widget buildSearchTile() {
     return Container(
+      alignment: Alignment.topCenter,
       height: 120.0,
-      padding: EdgeInsets.all(5.0),
+      margin: EdgeInsets.only(left: 5.0),
+      padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
       child: ListView(
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
